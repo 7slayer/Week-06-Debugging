57b21526baae3f1de67a8fbdaa76304720614c65
DoubleL <lancelierman@gmail.com>
2023-02-11 22:43:10 -0600

    Added PDF

--- src/Deck.java
+++ src/Deck.java
@@ -3,17 +3,19 @@ import java.util.Collections;
 import java.util.List;
 
 public class Deck {
-	
+	//Instantiates the suits array list that will be used to describe the card
 	private List<String> suits = new ArrayList<String>();
+	//This is the ArrayList that will be used to store the Card class
 	private List<Card> card = new ArrayList<Card>();
 	
 	public Deck() {
-		
+		//Sets the values of all the suits within a deck
 		suits.add("Clubs");
 		suits.add("Spades");
 		suits.add("Diamonds");
 		suits.add("Hearts");
 		
+		//cycles though the suits array list and creates 13 cards for each suit
 		for (int i = 0; i < 4; i++) {
 			for (int value = 2; value <= 14; value++) {
 				card.add(new Card(value, suits.get(i)));
@@ -26,7 +28,7 @@ public class Deck {
 	}
 	
 	public Card draw() {
-		
+		//Returns the value of the first card within the deck and removes it
 		Card drawnCard = card.get(0);
 		card.remove(0);
 		return drawnCard;
--- src/Player.java
+++ src/Player.java
@@ -2,13 +2,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-	
+	//Create a list of the newly created Card class to store the players hand
 	private List<Card> hand = new ArrayList<Card>();
+	//Creates the score variable to keep track of player score
 	private int score;
 	private String name;
 	
 	public Player(String name) {
-		
+		//Defines the constructor of the Player class by setting the name to the passed value and the score to zero
 		setName(name);
 		setScore(0);
 	}
@@ -28,17 +29,17 @@ public class Player {
 	private void setName(String name) {
 		this.name = name;
 	}
-	
+	//This will take a Card from the passed Deck and add it to the players hand
 	public void draw(Deck deckToDraw) {
 		hand.add(deckToDraw.draw());
 	}
-	
+	//This will remove the card in the players hand Array List and return the removed card
 	public Card flip() {
 		Card cardInHand = hand.get(0);
 		hand.remove(0);
 		return cardInHand;
 	}
-	
+	//Adds one point to the players score
 	public void incrementScore() {
 		setScore(getScore() + 1);
 	}
